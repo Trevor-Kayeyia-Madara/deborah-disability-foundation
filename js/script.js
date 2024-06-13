4dc9c4802cf56400d08b9645561d1f63c37b2590
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-06-13 19:41:17 +0300

    Added features

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-
 function showSuccessMessage() {
-    document.getElementById('success-message').style.display = 'block';
-  }
+  document.getElementById('success-message').style.display = 'block';
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  var fullName = document.getElementById('full_name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+  
+  var mailtoLink = `mailto:deborahfoundation2023@gmail.com?subject=Message%20from%20${encodeURIComponent(fullName)}&body=${encodeURIComponent(message)}%0A%0AFrom:%20${encodeURIComponent(fullName)}%20(${encodeURIComponent(email)})`;
+  
+  window.location.href = mailtoLink;
+
+  showSuccessMessage();
+});
